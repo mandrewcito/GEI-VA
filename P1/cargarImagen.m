2348652ecfbe2c5856eb4757ea0a9f97b9d97752
mandrewcito <anbaalo@gmail.com>
2014-11-01 17:28:25 +0100

    cargar img y nuevas img

--- P1/cargarImagen.m
+++ P1/cargarImagen.m
@@ -1,5 +1,9 @@
 function [ image ] = cargarImagen( imgsrc)
 %carga una img dada una ruta y la pasa a escala de grises
 f=imread(imgsrc);
+if size(f,3)==3,
 image=rgb2gray(f);
+elseif size(f,3)==1,
+image=f;
+end
 end
