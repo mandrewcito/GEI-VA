1434f7a592bfd92e1952344815cb9fc8104c3607
mandrewcito <anbaalo@gmail.com>
2015-01-03 12:05:38 +0100

    por puntos caracteristicos

--- P2/filtrar.py
+++ P2/filtrar.py
@@ -48,3 +48,43 @@ def estaCuadrante(coord1,region1,coord2,region2):
   if pt1x > mediox1 and pt1y > medioy1 and pt2x > mediox2 and pt2y > medioy2 :
     return True
   return False
+
+def filtrarKPCaracteristicas(kp_pairs,c1,c2):
+  cara1=c1[0][0]
+  xstart1=cara1[0]
+  ystart1=cara1[1]
+  cara2=c2[0][0]
+  xstart2=cara2[0]
+  ystart2=cara2[1]
+  newKP=[]
+  for puntos in kp_pairs:
+    boca1=c1[1][0]
+    region1=[boca1[1]+xstart1,boca1[0]+ystart1,boca1[3]+xstart1,boca1[2]+ystart1]
+    boca2=c2[1][0]
+    region2=[boca2[1]+xstart2,boca2[0]+ystart2,boca2[3]+xstart2,boca2[2]+ystart2]
+    if esta(puntos[0].pt,region1,puntos[1].pt,region2):
+      newKP.append(puntos)
+      continue
+    nariz1=c1[2][0]
+    region1=[nariz1[1]+xstart1,nariz1[0]+ystart1,nariz1[3]+xstart1,nariz1[2]+ystart1]
+    nariz2=c2[2][0]
+    region2=[nariz2[1]+xstart2,nariz2[0]+ystart2,nariz2[3]+xstart2,nariz2[2]+ystart2]
+    if esta(puntos[0].pt,region1,puntos[1].pt,region2):
+      newKP.append(puntos)
+      continue
+    ojo11=c1[3][0]
+    region1=[ojo11[1]+xstart1,ojo11[0]+ystart1,ojo11[3]+xstart1,ojo11[2]+ystart1]
+    ojo12=c1[3][1]
+    region2=[ojo12[1]+xstart2,ojo12[0]+ystart2,ojo12[3]+xstart2,ojo12[2]+ystart2]
+    if esta(puntos[0].pt,region1,puntos[1].pt,region2):
+      newKP.append(puntos)
+      continue
+    ojo21=c2[3][0]
+    region1=[ojo21[0]+xstart1,ojo21[1]+ystart1,ojo21[2]+xstart1,ojo21[3]+ystart1]
+    ojo22=c2[3][1]
+    region2=[ojo22[0]+xstart2,ojo22[1]+ystart2,ojo22[2]+xstart2,ojo22[3]+ystart2]
+    if esta(puntos[0].pt,region1,puntos[1].pt,region2):
+      newKP.append(puntos)
+      continue
+  print len(newKP)
+  return newKP
---
+++ P2/matchesCaraCaracteristicas.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+import cv2
+from find_obj import filter_matches,explore_match
+import constantes as c 
+import recortar as r
+from imagen import Imagen
+import filtrar as flt
+import facedetect as f
+import sys
+args = sys.argv[1:]
+sujeto1=args[0]
+sujeto2=args[1]
+im1=c.test+"/image-"+sujeto1+".jpg"
+im2=c.test+"/image-"+sujeto2+".jpg"
+img1 = cv2.imread(im1,0) # queryImage
+img2 = cv2.imread(im2,0) # trainImage
+# Initiate SIFT detector
+orb = cv2.ORB()
+
+# find the keypoints and descriptors with SIFT
+kp1, des1 = orb.detectAndCompute(img1,None)
+kp2, des2 = orb.detectAndCompute(img2,None)
+
+# create BFMatcher object
+bf = cv2.BFMatcher(cv2.NORM_HAMMING)#, crossCheck=True)
+#filtramos los kp por la zona de la cara para las dos imagenes 
+a,todo1,c1= f.detectFace(cv2.imread(im1),"im1")
+region1=c1[0][0]
+a,todo2,c2= f.detectFace(cv2.imread(im2),"im2")
+region2=c2[0][0]
+matches = bf.knnMatch(des1, trainDescriptors = des2, k = 2)
+p1, p2, kp_pairs = filter_matches(kp1, kp2, matches)
+print len(kp_pairs)
+kp_pairs=flt.filtrarKP(kp_pairs,region1,region2)
+print len(kp_pairs)
+#kp_pairs=flt.filtrarKPCuadrantes(kp_pairs,region1,region2)
+print len(kp_pairs)
+if todo1 and todo2: # si se detecto cara ojos y nariz en los dos ...
+  kp_pairs=flt.filtrarKPCaracteristicas(kp_pairs,c1,c2)
+if len(kp_pairs)!=0:
+  explore_match('find_obj', img1,img2,kp_pairs)#cv2 shows image
+if len(kp_pairs)>15:
+  print True
+else:
+  print False
+cv2.waitKey()
+cv2.destroyAllWindows()
